bd764a62e0083389377066d47d109ee5e5b92bd9
beecodeguy <beecodeguy@gmail.com>
2024-10-28 23:25:15 +0545

    new appointment with db post done

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,7 +1,41 @@
+import { AppointmentForm } from "@/components/forms/appointment-form";
+import { getPatient } from "@/lib/actions/patient.action";
+import Image from "next/image";
 import React from "react";
 
-const NewAppointmentRoute = () => {
-  return <div>NewAppointmentRoute</div>;
+const NewAppointmentRoute = async ({
+  params: { userId },
+}: SearchParamProps) => {
+  const patient = await getPatient(userId);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src={`/assets/icons/logo-full.svg`}
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <AppointmentForm
+            userId={userId}
+            type="create"
+            patientId={patient?.$id}
+          />
+          <p className="copyright mt-10 py-12">© 2024 CarePluse</p>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
 };
 
 export default NewAppointmentRoute;
---
+++ lib/actions/appointment.action.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import {
+  APPOINTMENTS_COLLECTION_ID,
+  DATABASE_ID,
+  databases,
+} from "@/appwrite.config";
+import { ID } from "node-appwrite";
+import { parseStringify } from "../utils";
+
+export const createAppointment = async (
+  appointment: CreateAppointmentParams
+) => {
+  try {
+    const newAppointment = await databases.createDocument(
+      DATABASE_ID!,
+      APPOINTMENTS_COLLECTION_ID!,
+      ID.unique(),
+      appointment
+    );
+    return parseStringify(newAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
